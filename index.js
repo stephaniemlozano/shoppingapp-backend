aa116e915036b68a290e1c5ad546927fe0d86af3
stephaniemlozano <stephaniesadowski@Stephanies-Air.home>
2022-08-24 21:25:17 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import cors from 'cors'
+import { MongoClient } from 'mongodb'
+import 'dotenv/config'
+
+const URI = process.env.MONGO_URI
+const client = new MongoClient(URI)
+const database = client.db('shopping-app')
+const products = database.collection('products')
+client.connect()
+console.log('connected to mongo')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.listen(4040, () => console.log('API running on port 4040'))
+
+
+app.get('/', async (request, response) => {
+  const allProducts = await products.find().toArray()
+  response.send(allProducts)
+})
